eca4f2983bae09546ded5dbf4d4a0dde2b1fd17f
vijay <vp.gd.msw@gmail.com>
2022-12-14 15:58:03 +0530

    set previous changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-netlify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  adapter: adapter()
+  plugins: [svelte()]
 })
